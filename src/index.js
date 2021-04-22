370698aa9984e90572775868a70c0a91ac430863
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-22 17:05:02 -0400

    commit tree 927212d5fe5de40f47397bdc30b1f19cbfe77b39

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  
+  fetch('http://localhost:3000/toys')
+  .then(r=>r.json()).then(toys => console.log(toys))
+
+// debugger
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
